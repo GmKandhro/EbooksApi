0ced0db84304b083f98c8f0551711531156d4f11
gmkandhro <gmkandhro182@gmail.com>
2025-05-30 07:11:26 +0500

    add cloudinary

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,6 +7,9 @@ const _config = {
     mongodbUrl: process.env.MONGODB_URL,
     env: process.env.NODE_ENV,
     jwtSecret: process.env.JWT_SCRECT,
+    cloudinary_cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+    cloudinary_apiKey: process.env.CLOUDINARY_API_KEY,
+    cloudinary_apiSecret: process.env.CLOUDINARY_API_SECRET,
 };
 
 export const config = Object.freeze(_config);
